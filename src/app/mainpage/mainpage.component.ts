7fc769a9d05a07159986fa3c025924aece05dfd0
hlaingwintunn <sv.hlaingwt@gmail.com>
2018-02-21 23:47:29 +0800

    init commit

---
+++ src/app/mainpage/mainpage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import '../../../node_modules/w3-css/w3.css';
+
+
+@Component({
+  selector: 'app-mainpage',
+  templateUrl: './mainpage.component.html',
+  styleUrls: ['./mainpage.component.css']
+})
+export class MainpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
