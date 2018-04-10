2b89456bbe974a1451b9fb81d9bcf48cdf927b48
hlaingwintun <sv.hlaingwt@gmail.com>
2018-04-10 23:57:10 +0800

    create modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { MainPageModule } from './mainpage/mainpage.module';
 
 import { AppComponent } from './app.component';
 import { PaymentComponent } from './payment/payment.component';
+import { CustomerComponent } from './customer/customer.component';
+import { ProductComponent } from './product/product.component';
+import { OrderComponent } from './order/order.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaymentComponent
+    PaymentComponent,
+    CustomerComponent,
+    ProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule, MainPageModule, FormsModule, HttpModule
